b20dc4f98d157a9bc4898beac2c1cd5ddd8fa541
georgejincy <jincydon101@gmail.com>
2016-12-05 13:15:54 -0500

    created gme characters and assigned them initial values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,35 @@
 
 //Game character
 
-function GameCharacter(healthPoints, attackPower, counterAttackPow) {
+function GameCharacter(healthPoints, attackPower, counterAttackPow, isYou) {
+
+	//properties
 	this.healthPoints = healthPoints;
 	this.attackPower = attackPower;
 	this.counterAttackPow = counterAttackPow;
+	this.isYou = isYou;
+	this.baseAttackPower = this.attackPower;
+
+	//methods
+	this.updAttackPow = function (){
+		if (this.isYou === true){
+			this.attackPower = this.attackPower + this.baseAttackPower;
+		}
+
+	};
+
+	this.updHealthPoints = function (attackPow){
+		this.healthPoints = this.healthPoints - attackPow;
+	};
+
+	this.checkHealth = function (){
+		if (this.healthPoints <= 0){
+			console.log("Defeated");
+		}
+	}
+
+	//Testing/Debugging
+	console.log("New character created: " + healthPoints + " " + attackPower + " " + counterAttackPow + " " + isYou + " ");
 	
 
 } //End of Object constructor
@@ -25,8 +50,15 @@ function GameCharacter(healthPoints, attackPower, counterAttackPow) {
 
 //create characters
 
-var capAmerica = new GameCharacter(150, 25, 30);
-var thor = new GameCharacter(180, 40, 50);
-var hulk = new GameCharacter(200, 50, 60);
-var ironMan = new GameCharacter(130, 45, 40);
-var blackWidow = new GameCharacter(120, 35, 55);
+var capAmerica = new GameCharacter(150, 25, 30, true);
+var thor = new GameCharacter(180, 40, 50, false);
+var hulk = new GameCharacter(200, 50, 60, false);
+var ironMan = new GameCharacter(130, 45, 40, false);
+var blackWidow = new GameCharacter(120, 35, 55, false);
+
+for (var i = 0; i < 5; i++) {
+	capAmerica.updAttackPow();
+
+	//Testing/Debugging
+	console.log("attack power: " + capAmerica.attackPower);
+}

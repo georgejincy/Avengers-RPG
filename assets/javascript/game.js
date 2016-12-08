856991cd9415ecee24fe4727ba6647a60edbac92
georgejincy <jincydon101@gmail.com>
2016-12-08 18:33:46 -0500

    changed layout of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,31 @@
 var you = "";
 var enemy = "";
 var isDefeated = false;
+var namesarray = ["capAmerica","thor","hulk","ironMan","blackWidow"];
+var hpArray = [150,180,200,130,120];
+var imgArray = [
+				"assets/images/capamerica.jpg",
+				"assets/images/thor.jpg",
+				"assets/images/hulk.jpg",
+				"assets/images/ironman.jpg",
+				"assets/images/blkwidow.jpg"];
+
+// array that stores defaults for game characters
+var charDefArray = [
+					{name: "capAmerica", healthP: 150, apower: 5, capower: 30},
+					{name: "thor", healthP: 180, apower: 12, capower: 20},
+					{name: "hulk", healthP: 200, apower: 15, capower: 25},
+					{name: "ironMan", healthP: 130, apower: 14, capower: 15},
+					{name: "blackWidow", healthP: 120, apower: 5, capower: 10}
+];
+//create an array of game character objects
+
+var charArray = [
+				new GameCharacter(150, 5, 30, false, "capAmerica"),
+				new GameCharacter(180, 12, 20, false, "thor"),
+				new GameCharacter(200, 15, 25, false, "hulk"),
+				new GameCharacter(130, 14, 15, false, "ironMan"),
+				new GameCharacter(120, 5, 10, false, "blackWidow")];
 
 //OBJECT
 //===================================================================================================
@@ -30,7 +55,6 @@ function GameCharacter(healthPoints, attackPower, counterAttackPow, isYou, charN
 	this.updHealthPoints = function (attackPow){
 		this.healthPoints = this.healthPoints - attackPow;
 		return this.healthPoints;
-		//$("p.healthpoints").html(this.healthPoints);
 	};
 
 	this.checkHealth = function (){
@@ -52,128 +76,6 @@ function GameCharacter(healthPoints, attackPower, counterAttackPow, isYou, charN
 
 // FUNCTIONS (These are bits of code that we will call upon to run when needed)
 // ==================================================================================================
-
-
-// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
-// ==================================================================================================
-
-var namesarray = ["capAmerica","thor","hulk","ironMan","blackWidow"];
-var hpArray = [150,180,200,130,120];
-var imgArray = [
-				"assets/images/capamerica.jpg",
-				"assets/images/thor.jpg",
-				"assets/images/hulk.jpg",
-				"assets/images/ironman.jpg",
-				"assets/images/blkwidow.jpg"];
-
-// array that stores defaults for game characters
-var charDefArray = [
-					{name: "capAmerica", healthP: 150, apower: 5, capower: 30},
-					{name: "thor", healthP: 180, apower: 12, capower: 20},
-					{name: "hulk", healthP: 200, apower: 15, capower: 25},
-					{name: "ironMan", healthP: 130, apower: 14, capower: 15},
-					{name: "blackWidow", healthP: 120, apower: 5, capower: 10}
-];
-
-
-//create an array of game character objects
-
-var charArray = [
-				new GameCharacter(150, 5, 30, false, "capAmerica"),
-				new GameCharacter(180, 12, 20, false, "thor"),
-				new GameCharacter(200, 15, 25, false, "hulk"),
-				new GameCharacter(130, 14, 15, false, "ironMan"),
-				new GameCharacter(120, 5, 10, false, "blackWidow")];
-
-/*var capAmerica = new GameCharacter(150, 25, 30, true);
-var thor = new GameCharacter(180, 40, 50, false);
-var hulk = new GameCharacter(200, 50, 60, false);
-var ironMan = new GameCharacter(130, 45, 40, false);
-var blackWidow = new GameCharacter(120, 35, 55, false);*/
-
-
-$("button").on("click", function(){
-
-	console.log("Button clicked");
-
-	if ($(this).hasClass("character")) {
-		console.log("A character has been clicked!");
-		console.log("character is: " + $(this).val());
-		you = $(this).val();
-		moveCharacters($(this).val());
-	}
-	else if($(this).hasClass("attack")){
-		console.log("Attack button has been clicked");
-
-		if ($("#defender").children().length === 0) {
-			//div has no other tags inside it
-			console.log("EMPTY");
-			$("#message").html("No enemy chosen here.");
-		}
-		else{
-			console.log(isDefeated);
-			if(isDefeated === false){
-				performAttack();
-			}
-		}
-
-	}
-
-	console.log("I'm at end of on click");
-
-});
-
-$("#enemies").on("click", "button", function(){
-
-	console.log("Button clicked");
-
-	/*if ($(this).hasClass("character")) {
-		console.log("A character has been clicked!");
-		console.log("character is: " + $(this).val());
-		moveCharacters($(this).val());
-	}*/
-	//move enemies to defender only if defender section is empty
-	if ($("#defender").children().length === 0) {
-		if($(this).hasClass("enemies")){
-			console.log("An enemy has been clicked!");
-			console.log("enemy is: " + $(this).val());
-			enemy = $(this).val();
-			moveCharToDef($(this).val());
-			$(this).remove();
-		}
-	}
-	else{
-		$("#message").html("You have already chosen an enemy. Click attack to play");
-	}
-
-	console.log("I'm at end of on click");
-
-});
-
-$("#message").on("click", "button", function(){
-
-	console.log("Restart Button clicked");
-
-	//clear all the divs and show characters div
-	$("#characters").show();
-	$("#yourchar").html("");
-	$("#enemies").html("");
-	$("#defender").html("");
-	$("#message").html("");
-	isDefeated = false;
-
-	//reset the properties of gamecharacter
-
-	for (var i = 0; i < charArray.length; i++) {
-		//capAmerica.updAttackPow();
-		charArray[i].healthPoints = charDefArray[i].healthP;
-		charArray[i].attackPower = charDefArray[i].apower;
-		//Testing/Debugging
-		console.log(charArray[i]);
-	}
-
-});
-
 function moveCharacters(charName){
 
 	console.log("I'm here");
@@ -183,8 +85,6 @@ function moveCharacters(charName){
 
 	//move character selected to your char panel
 	var charButton = createCharButton(charName);
-	/*var charButton = $("<button class='character'><img src = '' alt= 'character image'><p class = 'healthpoints'>Health points</p></button>"); 
-	charButton.addClass("." + charName);*/
 	charButton.addClass("yourChar");
 	charButton.appendTo("#yourchar");
 	
@@ -209,7 +109,6 @@ function moveCharacters(charName){
 }
 
 function moveCharToDef(charName){
-	//$("." + charName).hide();
 
 	//move character selected to your Defender panel
 	var charButton = createCharButton(charName);
@@ -301,17 +200,94 @@ function performAttack(){
 		$("#message").append("<p> You attacked " + enemy + " for  " + youAttackpow + "  damage.</p>"); 
 		$("#message").append("<p> " + enemy + " attacked you back for  " + enemyAttackPow + "  damage.</p>"); 
 	}
-	//$("#message").html("hello");
-
-
 }
+
+// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
+// ==================================================================================================
+
+$("button").on("click", function(){
+
+	console.log("Button clicked");
+
+	if ($(this).hasClass("character")) {
+		console.log("A character has been clicked!");
+		console.log("character is: " + $(this).val());
+		you = $(this).val();
+		moveCharacters($(this).val());
+	}
+	else if($(this).hasClass("attack")){
+		console.log("Attack button has been clicked");
+
+		if ($("#defender").children().length === 0) {
+			//div has no other tags inside it
+			console.log("EMPTY");
+			$("#message").html("No enemy chosen here.");
+		}
+		else{
+			console.log(isDefeated);
+			if(isDefeated === false){
+				performAttack();
+			}
+		}
+
+	}
+
+	console.log("I'm at end of on click");
+
+});
+
+$("#enemies").on("click", "button", function(){
+
+	console.log("Button clicked");
+
+	//move enemies to defender only if defender section is empty
+	if ($("#defender").children().length === 0) {
+		if($(this).hasClass("enemies")){
+			console.log("An enemy has been clicked!");
+			console.log("enemy is: " + $(this).val());
+			enemy = $(this).val();
+			moveCharToDef($(this).val());
+			$(this).remove();
+		}
+	}
+	else{
+		$("#message").html("You have already chosen an enemy. Click attack to play");
+	}
+
+	console.log("I'm at end of on click");
+
+});
+
+$("#message").on("click", "button", function(){
+
+	console.log("Restart Button clicked");
+
+	//clear all the divs and show characters div
+	$("#characters").show();
+	$("#yourchar").html("");
+	$("#enemies").html("");
+	$("#defender").html("");
+	$("#message").html("");
+	isDefeated = false;
+
+	//reset the properties of gamecharacter
+
+	for (var i = 0; i < charArray.length; i++) {
+		
+		charArray[i].healthPoints = charDefArray[i].healthP;
+		charArray[i].attackPower = charDefArray[i].apower;
+		//Testing/Debugging
+		console.log(charArray[i]);
+	}
+
+});
+
 
 //Testing/Debugging
 
 function displayCharArray(){
 
 	for (var i = 0; i < charArray.length; i++) {
-		//capAmerica.updAttackPow();
 
 		//Testing/Debugging
 		console.log(charArray[i]);
@@ -326,9 +302,4 @@ $.each(charDefArray, function() {
 	});
 });
 
-/*for (var i = 0; i < 5; i++) {
-	capAmerica.updAttackPow();
 
-	//Testing/Debugging
-	console.log("attack power: " + capAmerica.attackPower);
-}*/
